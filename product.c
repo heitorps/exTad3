398d83c7be4068f17cb2aa334561bb7f97bc08b2
HeitorPinheiroSousa <heitorps2020@gmail.com>
2024-04-02 22:16:39 -0300

    first commit

---
+++ product.c
@@ -0,0 +1,124 @@
+#include "product.h"
+
+
+#define MAXTAM_NAME 64
+
+struct product
+{
+    char name[MAXTAM_NAME];
+    float price;
+    float discount;
+    int qtd;
+    int sales;
+};
+
+Product *product_constructor(const char *name, float price, int qtd){
+
+    if(price < 0 || qtd < 0){
+        printf("VALOR INVALIDO\n");
+        return NULL;
+    }
+
+    Product* p = (Product*)calloc(1,sizeof(Product));
+
+    strcpy(p->name,name);
+    p->price = price;
+    p->qtd = qtd;
+
+    return p;
+}
+
+const char *product_get_name(Product *p){
+    return p->name;
+}
+float product_get_price(Product *p){
+    return p->price;
+}
+float product_get_discount(Product *p){
+    return p->discount;
+}
+int product_get_qtd(Product *p){
+    return p->qtd;
+}
+int product_get_sales(Product *p){
+    return p->sales;
+}
+
+void product_set_name(Product *p, const char *name){
+    strcpy(p->name,name);
+}
+void product_set_price(Product *p, float price){
+    if(price < 0){
+        printf("VALOR INVALIDO\n");
+        return;
+    }
+    p->price = price;
+}
+void product_set_discount(Product *p, float discount){
+    if(discount < 0 || discount > 1){
+        printf("VALOR INVALIDO\n");
+        return;
+    }
+    p->discount = discount;
+}
+
+void product_sell(Product *p, int quantity){
+    if(quantity > p->qtd){
+        printf("ESTOQUE INSUFICIENTE\n");
+        return;
+    }
+    if(quantity<0){
+        printf("QUANTIDADE INVALIDA\n");
+        return;
+    }
+    p->qtd -= quantity;
+    p->sales += quantity;
+}
+void product_buy(Product *p, int quantity){
+    if(quantity<0){
+        printf("QUANTIDADE INVALIDA\n");
+        return;
+    }
+    
+    p->qtd += quantity;
+}
+
+float product_get_price_with_discount(Product *p){
+    float pwd = p->price * (1.0 - p->discount);
+    
+    return pwd;
+}
+void product_print(Product *p){
+    printf("Product(%s, %.2f, %.2f, %.2f, %d, %d)\n",
+                p->name, p->price, p->discount,
+                product_get_price_with_discount(p),p->qtd, p->sales);
+}
+
+void product_destructor(Product *p){
+    if(p!=NULL) free(p);
+}
+
+int product_compare_name(const void* produto1, const void* produto2){
+    Product* p1 = *((Product**)produto1);
+    Product* p2 = *((Product**)produto2);
+
+    return strcmp(p1->name,p2->name);
+}
+
+int product_compare_price(const void* produto1, const void* produto2){
+    Product* p1 = *((Product**)produto1);
+    Product* p2 = *((Product**)produto2);
+
+    if(p1->price > p2->price) return 1;
+    if(p1->price < p2->price) return -1;
+    return 0;
+}
+
+int product_compare_sales(const void* produto1, const void* produto2){
+    Product* p1 = *((Product**)produto1);
+    Product* p2 = *((Product**)produto2);
+
+    if(p1->sales > p2->sales) return 1;
+    if(p1->sales < p2->sales) return -1;
+    return 0;
+}
---
+++ vector.c
@@ -0,0 +1,201 @@
+#include "vector.h"
+#include "stdlib.h"
+
+// Aloca espaço para um elemento do tipo vector e inicializa os seus atributos.
+Vector *vector_construct(){
+    Vector* v = (Vector*)malloc(sizeof(Vector));
+    v->size = 0;
+    v->allocated = 10;
+
+    v->data = (data_type*)calloc(v->allocated,sizeof(data_type));
+
+    return v;
+}
+
+// Libera o espaço reservado para o vector.
+void vector_destroy(Vector *v){
+    if(v->data != NULL) free(v->data);
+
+    if(v != NULL)   free(v);
+}
+
+// Adiciona o item dado ao final do vector e aumenta o tamanho. Aumenta a capacidade alocada se necessário.
+void vector_push_back(Vector *v, data_type val){
+    v->size++;
+    if(v->size > v->allocated){
+        v->allocated *= 2;
+        v->data = (data_type*)realloc(v->data,v->allocated*sizeof(data_type));
+    }
+    int i = v->size - 1;
+    v->data[i] = val;
+}
+
+// Retorna o número de elementos atualmente no vector.
+int vector_size(Vector *v){
+    return v->size;
+}
+
+// Retorna o i-ésimo elemento do vector.
+data_type vector_get(Vector *v, int i){
+    if(i>=0 && i < v->size)
+    return v->data[i];
+
+    else exit(1);
+}
+
+// Atualiza o i-ésimo elemento do vector  para que ele passe a ter o valor val.
+void vector_set(Vector *v, int i, data_type val){
+    if(i>=0 && i < v->size)
+    v->data[i] = val;
+}
+
+// Retorna o índice da primeira ocorrência do item no Vector ou -1 se o elemento não for encontrado.
+int vector_find(Vector *v, data_type val){
+    for(int i=0; i<v->size; i++){
+        if(v->data[i] == val) return i;
+    } return -1;
+}
+
+// Retorna o maior elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
+data_type vector_max(Vector *v){
+    data_type max = v->data[0];
+
+    for(int i=0; i<v->size; i++){
+        if(v->data[i] > max) max = v->data[i];
+    }
+    return max;
+}
+
+// Retorna o menor elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
+data_type vector_min(Vector *v){
+    data_type min = v->data[0];
+    
+    for(int i=0; i<v->size; i++){
+        if(v->data[i] < min) min = v->data[i];
+    }
+    return min;
+}
+
+// Retorna o índice do maior elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
+int vector_argmax(Vector *v){
+    data_type max = v->data[0];
+    int idxMax = 0;
+    
+    for(int i=0; i<v->size; i++){
+        if(v->data[i] > max){
+            max = v->data[i];
+            idxMax = i;
+        }
+    }
+    return idxMax;
+}
+
+// Retorna o índice do menor elemento do vector (assumindo que podemos comparar elementos usando o operador "<")
+int vector_argmin(Vector *v){
+    data_type min = v->data[0];
+    int idxMin = 0;
+    
+    for(int i=0; i<v->size; i++){
+        if(v->data[i] < min){
+            min = v->data[i];
+            idxMin = i;
+        }
+    }
+    return idxMin;
+}
+
+/////////////////////////////////////////////////////////////////////////
+// Remove o i-ésimo elemento do vetor.
+data_type vector_remove(Vector *v, int i){
+    data_type banido = v->data[i];
+    for(int j=i+1; j< v->size; j++){
+        v->data[j-1] = v->data[j];
+    }
+    v->size--;
+    return banido;
+}
+
+// Remove o primeiro elemento
+data_type vector_pop_front(Vector *v){
+    return vector_remove(v,0);
+}
+
+// Remove o ultimo elemento
+data_type vector_pop_back(Vector *v){
+    return vector_remove(v,v->size-1);
+}
+
+// Insere o elemento na i-esima posicao
+void vector_insert(Vector *v, int i, data_type val){
+    //aumento de tamanho
+    v->size++;
+    if(v->size > v->allocated){
+        v->allocated *= 2;
+        v->data = (data_type*)realloc(v->data,v->allocated*sizeof(data_type));
+    }
+
+    //passagem de todos a direita
+    for(int j = v->size-1;    j > i;  j--){
+        v->data[j] = v->data[j-1];
+    }
+    v->data[i] = val;
+}
+
+// Troca os elementos das posições i e j (i vira j e j vira i)
+void vector_swap(Vector *v, int i, int j){
+    data_type temp;
+
+    temp = v->data[i];
+    v->data[i] = v->data[j];
+    v->data[j] = temp;
+}
+
+// Ordena o vetor in-place (sem criar um novo vetor)
+void vector_sort(Vector *v){
+    while(1){
+        int nSwaps=0;
+
+        for(int i=0; i < v->size-1; i++){
+            if(v->data[i]>v->data[i+1]){
+                vector_swap(v,i,i+1);
+                nSwaps++;
+            }
+        }
+        if (!nSwaps) break;
+    }
+}
+
+// Retorna o indice de val usando busca binaria. Retorna -1 se nao encontrado.
+int vector_binary_search(Vector *v, data_type val){
+    int minIdx=0, maxIdx = v->size-1;
+
+    int medIdx;
+    while (minIdx <= maxIdx)
+    {
+        medIdx = (minIdx + maxIdx)/2;
+
+        if(v->data[medIdx] == val) return medIdx;
+        else if(val > v->data[medIdx]){
+            minIdx = medIdx+1;
+        }
+        else{
+            maxIdx = medIdx-1;
+        }
+
+    }
+    return -1;
+    
+}
+
+// Inverte o vetor in-place (sem criar um novo vetor)
+void vector_reverse(Vector *v){
+    for(int i=0; i<(v->size)/2; i++){
+        vector_swap(v,i,(v->size-1-i));
+    }
+}
+
+// Cria uma cópia do vector e dos valores de seus atributos.
+Vector *vector_copy(Vector *v);
+
+// Remove todos os elementos de v
+void vector_clear(Vector *v);
